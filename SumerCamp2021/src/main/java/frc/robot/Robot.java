ad0ac71b8837e745e21f303963dde13b932dd5ba
kcjesse02 <32419147+kcjesse02@users.noreply.github.com>
2021-06-06 13:23:36 -0400

    Added Servo Function
    Added the basis for Servo functions. Having problems with the Romi detecting the Servo input.

--- SumerCamp2021/src/main/java/frc/robot/Robot.java
+++ SumerCamp2021/src/main/java/frc/robot/Robot.java
@@ -3,10 +3,13 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Servo;
 
 /**
  * The VM is configured to automatically run this class, and to call the run() function when the
@@ -18,8 +21,10 @@ public class Robot extends EducationalRobot {
   private final Spark m_leftMotor = new Spark(0);
   private final Spark m_rightMotor = new Spark(1);
 
+
   // Assumes a gamepad plugged into channnel 0
-  private final Joystick m_controller = new Joystick(0);
+  private final XboxController m_controller = new XboxController(0);
+  private final Servo m_servo = new Servo(5);
  
 
  // Set up the differential drive controller
@@ -50,6 +55,16 @@ public class Robot extends EducationalRobot {
     while (m_ds.isEnabled()){
       // m_diffDrive.arcadeDrive(-m_controller.getRawAxis(3), m_controller.getRawAxis(2));
       m_diffDrive.tankDrive(-m_controller.getRawAxis(1), m_controller.getRawAxis(3));
+      if(m_controller.getXButton()==true){
+        m_servo.set(0.5);
+        System.out.println("0.5");
+      }
+      if(m_controller.getYButton()==true){
+        m_servo.set(0);
+        System.out.println("0");
+      }
+      }
+      
     }
 
   }

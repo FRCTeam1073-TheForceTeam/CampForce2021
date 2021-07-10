530d92a7c95727afe5a223b53c344021d751b279
kcjesse02 <32419147+kcjesse02@users.noreply.github.com>
2021-07-10 09:38:10 -0400

    added sensor voltage to distance converter

--- SumerCamp2021/src/main/java/frc/robot/Robot.java
+++ SumerCamp2021/src/main/java/frc/robot/Robot.java
@@ -3,10 +3,13 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Servo;
 
 /**
  * The VM is configured to automatically run this class, and to call the run() function when the
@@ -18,8 +21,12 @@ public class Robot extends EducationalRobot {
   private final Spark m_leftMotor = new Spark(0);
   private final Spark m_rightMotor = new Spark(1);
 
+
   // Assumes a gamepad plugged into channnel 0
-  private final Joystick m_controller = new Joystick(0);
+  private final XboxController m_controller = new XboxController(0);
+  //make sure to check ID with the romi webpage
+  private final Servo m_servo = new Servo(4);
+  AnalogInput dist = new AnalogInput(0);
  
 
  // Set up the differential drive controller
@@ -29,6 +36,11 @@ public class Robot extends EducationalRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+
+   public static double voltageToDist(double voltage){
+
+    return (17.8333/Math.pow(voltage, 0.717466)) - 5.16699;
+   }
   @Override
   public void robotInit() {
     m_diffDrive.setSafetyEnabled(false);
@@ -39,18 +51,47 @@ public class Robot extends EducationalRobot {
   @Override
   public void run() {
     
-      //System.out.print(m_leftMotor.getSpeed() + " ");
-   
-    m_diffDrive.arcadeDrive(1.0, 0.0);
-    Timer.delay(0.5);
-    m_diffDrive.arcadeDrive(0.0, 0.4);
+   if(m_ds.isAutonomousEnabled()){
+    
+    m_diffDrive.tankDrive(0, 0);
+    while(Robot.voltageToDist(dist.getAverageVoltage()) >= 5.9){
+      m_diffDrive.tankDrive(0.5, 0.5);
+    }
+    m_diffDrive.tankDrive(0, 0);
+    m_servo.set(0.25);
+    /*m_servo.set(0.5);
+    Timer.delay(1);
+    m_servo.set(0.5);*/
+    
+  
+ 
+   }
+   while(m_ds.isAutonomousEnabled()){
+    System.out.println(Robot.voltageToDist(dist.getAverageVoltage()));
     Timer.delay(0.25);
-    m_diffDrive.arcadeDrive(0.0, 0.0);
+   }
 
-    while (m_ds.isEnabled()){
-      // m_diffDrive.arcadeDrive(-m_controller.getRawAxis(3), m_controller.getRawAxis(2));
-      m_diffDrive.tankDrive(-m_controller.getRawAxis(1), m_controller.getRawAxis(3));
+    while (m_ds.isOperatorControlEnabled()){
+      
+      //drive code 
+      //m_diffDrive.arcadeDrive(m_controller.getRawAxis(1), m_controller.getRawAxis(4), true);
+      m_diffDrive.tankDrive(m_controller.getRawAxis(1), m_controller.getRawAxis(3), false);
+      
+      
+      //servo code
+      if(m_controller.getXButton()==true){
+        m_servo.set(0.5);
+        System.out.println("0.5");
+      }
+
+      if(m_controller.getYButton()==true){
+        m_servo.set(0.0);
+        System.out.println("0");
+      }
+      
+      }
+      
     }
 
   }
-}
+
